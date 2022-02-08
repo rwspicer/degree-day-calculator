1a5132d6d93a6430a2839eda0775ec2f85c05532
Rawser Spicer <rwspicer@alaska.edu>
2022-02-08 13:50:50 -0900

    added restart features; added options for saving/loading multigrids

--- ddc/utility.py
+++ ddc/utility.py
@@ -5,7 +5,7 @@ Degree-Day Calculator
 CLI utility for calculating degree-days from monthly data
 """
 import glob
-import os
+import os, sys
 from datetime import datetime
 
 import numpy as np
@@ -75,7 +75,7 @@ def utility ():
     python ddc/utility.py 
         --in-temperature=../tas_mean_C_AK_CAN_AR5_5modelAvg_rcp45_01_2006-12_2100
         --out-fdd=./fdd --out-tdd=./tdd --start-year=2006 --mask-val=-9999 
-        --num-processes=6 --verbose=log --sort_method=snap
+        --num-processes=6 --verbose=log --sort-method=snap
 
     """
  
@@ -89,14 +89,16 @@ def utility ():
             
             
             ],
-            ['--num-processes', 
-             '--mask-val', 
-             '--verbose',
-             '--temp-dir',
-             '--sort-method',
-             '--logging-dir',
-             '--out-roots',
-             '--out-format'
+            [
+                '--num-processes', 
+                '--mask-val', 
+                '--verbose',
+                '--temp-dir',
+                '--sort-method',
+                '--logging-dir',
+                '--out-roots',
+                '--out-format',
+                '--start-at'
             ]
         
         )
@@ -113,6 +115,7 @@ def utility ():
         verbosity = 0
     # print(verbosity)
     # return
+    # verbosity =
     sort_method = "Using default sort function"
     sort_fn = sorted
 
@@ -135,72 +138,111 @@ def utility ():
         print('Seting up input...')
         print('\t', sort_method)
     
+    try: 
+        os.makedirs(arguments['--out-fdd'])
+    except:
+        pass
+    try: 
+        os.makedirs(arguments['--out-tdd'])
+    except:
+        pass
+
+
+
+    if arguments['--out-roots']:
+        try: 
+            os.makedirs(arguments['--out-roots'])
+        except:
+            pass
+    
 
     start_year = int(arguments['--start-year'])
-    months = [
-        '01','02','03','04','05','06',
-        '07','08','09','10','11','12',
-    ]
+    # months = [
+    #     '01','02','03','04','05','06',
+    #     '07','08','09','10','11','12',
+    # ]
 
     num_processes = int(arguments['--num-processes']) \
         if arguments['--num-processes']  else 1
-    num_years = len(
+    
+    if os.path.isfile(arguments['--in-temperature']):
+        print('in file', arguments['--in-temperature'])
+        monthly_temps = TemporalGrid(arguments['--in-temperature'])
+        print(monthly_temps)
+        num_years = monthly_temps.config['num_timesteps'] // 12
+        raster_metadata  = monthly_temps.config['raster_metadata'] 
+    else:
+        num_years = len(
             glob.glob(os.path.join(arguments['--in-temperature'],'*.tif')) 
         ) 
-    num_years = num_years // 12
+        num_years = num_years // 12
 
-    years = [start_year + yr for yr in range(num_years)]
+        years = [start_year + yr for yr in range(num_years)]
 
+        temporal_grid_keys = [] 
+        for yr in years: 
+            for mn in range(1,13):
+                temporal_grid_keys.append('%d-%02d' % (yr,mn) ) 
 
-    temporal_grid_keys = [] 
-    for yr in years: 
-        for mn in months: 
-            temporal_grid_keys.append(str(yr) + '-' + str(mn)) 
-
-    load_params = {
-            "method": "tiff",
-            "directory": arguments['--in-temperature'],
-            "sort_func": sort_fn,
-            "verbose": True if verbosity >= 2 else False
+        load_params = {
+                "method": "tiff",
+                "directory": arguments['--in-temperature'],
+                "sort_func": sort_fn,
+                "verbose": True if verbosity >= 2 else False
+            }
+        create_params = {
+            "name": "monthly temperatures",
+            # "start_timestep": int(arguments['--start-year']),
+            "grid_names": temporal_grid_keys
+            
         }
-    create_params = {
-        "name": "monthly temperatures",
-        # "start_timestep": int(arguments['--start-year']),
-        "grid_names": temporal_grid_keys
+        monthly_temps = load_and_create(load_params, create_params)
         
-    }
-    monthly_temps = load_and_create(load_params, create_params)
-    
-    ex_raster = glob.glob(
-            os.path.join(arguments['--in-temperature'],'*.tif')
-        )[0]
-    raster_metadata = get_raster_metadata(ex_raster)
-    monthly_temps.config['raster_metadata'] = raster_metadata
+        ex_raster = glob.glob(
+                os.path.join(arguments['--in-temperature'],'*.tif')
+            )[0]
+        raster_metadata = get_raster_metadata(ex_raster)
+        monthly_temps.config['raster_metadata'] = raster_metadata
 
-    mask_val = -3.39999999999999996e+38
-#    if arguments['--mask-val'] is None:
-#        mask_val = int(arguments['--mask-val'])
+        mask_val = -3.39999999999999996e+38
+    #    if arguments['--mask-val'] is None:
+    #        mask_val = int(arguments['--mask-val'])
 
-
-    idx = monthly_temps.grids < -1000
-    monthly_temps.grids[idx] = np.nan
-
+        idx = monthly_temps.grids < -1000
+        monthly_temps.grids[idx] = np.nan
 
 
     grid_shape = monthly_temps.config['grid_shape']
+    print('a', grid_shape[0], grid_shape[1], num_years)
     # out_shape = (num_years, grid_shape[0], grid_shape[1])
-    fdd = TemporalGrid(
-        grid_shape[0], grid_shape[1], num_years, 
-        start_timestep=start_year,
-        dataset_name = 'fdd',
-        mode='w+'
-    )
-    tdd = TemporalGrid(
-        grid_shape[0], grid_shape[1], num_years, 
-        start_timestep=start_year,
-        dataset_name = 'tdd',
-        mode='w+'
-    )
+    if  os.path.isfile(os.path.join(arguments['--out-fdd'], 'fdd.yml')):
+        fdd = TemporalGrid(os.path.join(arguments['--out-fdd'], 'fdd.yml'))
+    else:
+        fdd = TemporalGrid(
+            grid_shape[0], grid_shape[1], num_years, 
+            start_timestep=start_year,
+            dataset_name = 'fdd',
+            mode='w+'
+        )
+        fdd.config['raster_metadata'] = raster_metadata
+        fdd.config['dataset_name'] = 'freezing degree-day'
+        fdd.save(os.path.join(arguments['--out-fdd'], 'fdd.yml'))
+        fdd = TemporalGrid(os.path.join(arguments['--out-fdd'], 'fdd.yml'))
+    print('b')
+    if  os.path.isfile(os.path.join(arguments['--out-tdd'], 'tdd.yml')):
+        tdd = TemporalGrid(os.path.join(arguments['--out-tdd'], 'tdd.yml'))
+    else:
+        
+        tdd = TemporalGrid(
+            grid_shape[0], grid_shape[1], num_years, 
+            start_timestep=start_year,
+            dataset_name = 'tdd',
+            mode='w+'
+        )
+        tdd.config['raster_metadata'] = raster_metadata
+        tdd.config['dataset_name'] = 'thawing degree-day'
+        tdd.save(os.path.join(arguments['--out-tdd'], 'tdd.yml'))
+        tdd = TemporalGrid(os.path.join(arguments['--out-tdd'], 'tdd.yml'))
 
     roots = TemporalGrid(
         grid_shape[0], grid_shape[1], num_years*2, 
@@ -212,7 +254,10 @@ def utility ():
 
     
     days = create_day_array( 
-        [datetime.strptime(d, '%Y-%m') for d in temporal_grid_keys] 
+        [ datetime.strptime(d, '%Y-%m') for d in list(
+            monthly_temps.config['grid_name_map'].keys()
+            )
+        ] 
     )
     shape = monthly_temps.config['memory_shape']
 
@@ -235,6 +280,7 @@ def utility ():
             tdd.grids, 
             fdd.grids, 
             grid_shape, 
+            start= int(arguments['--start-at']) if arguments['--start-at'] else 0,
             num_process = num_processes,
             log=log,
             roots_grid=roots.grids,
@@ -275,6 +321,7 @@ def utility ():
             arguments['--out-format'] == 'tiff' :
         fdd.save_all_as_geotiff(arguments['--out-fdd'])
     elif arguments['--out-format'] == 'multigrid':
+        fdd.config['command-used-to-create'] = ' '.join(sys.argv)
         fdd.save(os.path.join(arguments['--out-fdd'], 'fdd.yml'))
 
 
@@ -284,7 +331,10 @@ def utility ():
     if arguments['--out-format'] is None or \
             arguments['--out-format'] == 'tiff' :
         tdd.save_all_as_geotiff(arguments['--out-tdd'])
+        
+
     elif arguments['--out-format'] == 'multigrid':
+        tdd.config['command-used-to-create'] = ' '.join(sys.argv)
         tdd.save(os.path.join(arguments['--out-tdd'], 'tdd.yml'))
 
 
@@ -296,7 +346,9 @@ def utility ():
             arguments['--out-format'] == 'tiff' :
             roots.save_all_as_geotiff(arguments['--out-roots'])
         elif arguments['--out-format'] == 'multigrid':
+            roots.config['command-used-to-create'] = ' '.join(sys.argv)
             roots.save(os.path.join(arguments['--out-roots'], 'roots.yml'))
+
         # roots.save('./out_roots.yml')
 
 ## fix this
